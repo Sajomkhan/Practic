7093072de95c14a46b59de6598e14c5f36de9fc2
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2023-02-02 21:08:18 +0600

    Update index.js

--- time-greeting/index.js
+++ time-greeting/index.js
@@ -1,5 +1,17 @@
-let text = "";
+// time tracking
+let hours = new Date().getHours();
+const greetPlace = document.getElementById('demo')
 
+let greet;
+if(hours < 12)greet = 'good morning'
+if(hours >= 12 && hours <= 17) greet = "good after noon"
+if(hours > 17 && hours <= 24) greet = `good evening`
+
+greetPlace.innerHTML = greet;
+
+
+//for loop
+let text = "";
 for (let i = 0; i < 5; i++) {
   text += "The number is " + i + "<br>";
 }
